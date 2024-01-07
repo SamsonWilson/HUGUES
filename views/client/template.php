7b6d8b06c53022442a0e68a3ec0cf9befd60425d
hugues <hugueswilson16@mail.com>
2024-01-07 20:10:06 +0900

    suit

--- views/client/template.php
+++ views/client/template.php
@@ -9,7 +9,7 @@
 <body>
 
 
-    <div class="container">
+    <div class="#">
         <!--<h1 class=" rounded border border-dark p-2 m-2 text-center text-white bg-info"> <?= $titre ?> </h1> -->
         <?= $client ?>
     </div>
